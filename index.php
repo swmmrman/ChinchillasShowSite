dc5fc0035f02b8f5e72d8b2ce1ae953d798cfac9
Stephan Kern <stephan@swmmrman.com>
2021-02-18 00:59:50 -0700

    Page to catch improper configuration of webserver and give instructions.

---
+++ index.php
@@ -0,0 +1,44 @@
+<?php
+$currentDir = $_SERVER[DOCUMENT_ROOT]
+?>
+<!doctype html>
+<html lang="en">
+  <header>
+    <title>You should not see this</title>
+  </header>
+  <body>
+    <p>
+      This page should not be visible, please set your document root to the
+      public html direction.
+    </p>
+    <h2>Apache Ex.</h2>
+    <pre>
+&lt;VirtualHost *:80&gt;
+  ServerName showsite
+  DocumentRoot <?= $currentDir ?>/public_html/
+  &lt;Directory <?= $currentDir ?>/public_html/&gt;
+    Require all granted
+    Options +Indexes
+  &lt;/Directory&gt;
+&lt;/VirtualHost&gt;
+    </pre>
+    <h2>Nginx Ex.</h2>
+    <pre>
+server {
+  listen 80;
+  listen [::]:80;
+
+  server_name showsite;
+
+  root '<?= $currentDir ?>/publichtml/';
+  index index.php;
+  location / {
+    tryfiles &uri $uri/ =404;
+  }
+  location ~ \.php$ {
+    include snippets/fastcgi-php.conf;
+    fastcgi_pass unix:/run/php/php7.4-fpm.sock;
+  }
+}
+    </pre>
+</html>
