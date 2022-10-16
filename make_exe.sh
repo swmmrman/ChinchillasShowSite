88e5c5eefc6b1231c79795895dfa31319f76ccd1
Stephan Kern <stephan@swmmrman.com>
2022-10-16 00:34:20 -0600

    Add a way to quickly make a windows exe

---
+++ make_exe.sh
@@ -0,0 +1,17 @@
+rm -r target/x84_64-pc-windows-gnu/release/
+cargo build --target x86_64-pc-windows-gnu --release
+if [ -e chinchillas_show_site ]; then
+    rm -r chinchillas_show_site
+    mkdir chinchillas_show_site
+fi
+
+cp -r public_html chinchillas_show_site
+cp -r template chinchillas_show_site
+cp target/x86_64-pc-windows-gnu/release/chinchillas_show_site.exe chinchillas_show_site/
+
+if [ -e "chinshow.zip" ]; then
+    rm chinshow.zip
+fi
+
+pushd chinchillas_show_site
+zip chinchillas_show_site.zip -r *
