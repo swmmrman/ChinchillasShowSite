d9b19f142bc0030d8b788db02a65f723634c8f80
Stephan Kern <stephan@swmmrman.com>
2022-10-15 19:28:49 -0600

    Initial cargo setup

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
-fn main() {
-    println!("Hello, world!");
+#[macro_use] extern crate rocket;
+
+#[get("/index.php")]
+fn index() -> &'static str {
+    "Hello"
+}
+
+
+#[launch]
+fn rocekt() -> _ {
+    rocket::build()
+        .mount("/", routes![index])
 }
