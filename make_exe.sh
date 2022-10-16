9ff42ce89253e99619944e0193dabfec1100eeb5
Stephan Kern <stephan@swmmrman.com>
2022-10-16 01:00:26 -0600

    Fix windows maker so folder structure is correct and distinct from linux build

--- make_exe.sh
+++ make_exe.sh
@@ -1,20 +1,20 @@
 #!/bin/bash
 
 cargo build --target x86_64-pc-windows-gnu --release
-if [ -e chinchillas_show_site ]; then
-    rm -r chinchillas_show_site
-    mkdir chinchillas_show_site
+if [ -e chinchillas_show_site-windows ]; then
+    rm -r chinchillas_show_site-windows
 fi
 
-cp -r public_html chinchillas_show_site
-cp -r template chinchillas_show_site
+mkdir chinchillas_show_site-windows
+cp -r public_html chinchillas_show_site-windows
+cp -r template chinchillas_show_site-windows
 cp target/x86_64-pc-windows-gnu/release/chinchillas_show_site.exe chinchillas_show_site/
 
 if [ -e "chinshow.zip" ]; then
     rm chinshow.zip
 fi
 
-pushd chinchillas_show_site
+pushd chinchillas_show_site-windows
 zip chinchillas_show_site.zip -r *
 mv chinchillas_show_site.zip ../
 popd
