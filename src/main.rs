638d2ed1ba63951306a2135c1a37ebd48199e4d3
Stephan Kern <stephan@swmmrman.com>
2022-10-15 19:53:12 -0600

    Add JS method

--- src/main.rs
+++ src/main.rs
@@ -8,14 +8,19 @@ async fn css(css_file: &str) -> Option<NamedFile> {
     NamedFile::open(Path::new("css/").join(css_file)).await.ok()
 }
 
-#[get("/index.php")]
+#[get("/")]
 fn index() -> &'static str {
     "Hello"
+}
+
+#[get("/js/<js_file>")]
+async fn js(js_file: &str) -> Option<NamedFile> {
+    NamedFile::open(Path::new("js").join(js_file)).await.ok()
 }
 
 
 #[launch]
 fn rocekt() -> _ {
     rocket::build()
-        .mount("/", routes![index, css])
+        .mount("/", routes![index, css, js])
 }
