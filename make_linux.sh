2b41c76efe82de8a1458f2ae8f90f764a7e9eeac
Stephan Kern <stephan@swmmrman.com>
2022-10-16 01:01:10 -0600

    Linux shell script to generate a distributable tar.gz

---
+++ make_linux.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+cargo build --release
+if [ -e chinchillas_show_site-linux ]; then
+    rm -r chinchillas_show_site-linux
+fi
+
+mkdir chinchillas_show_site-linux
+cp -r public_html chinchillas_show_site-linux
+cp -r template chinchillas_show_site-linux
+cp target/release/chinchillas_show_site chinchillas_show_site-linux/
+
+if [ -e chinchillas_show_site.tar.gz ]; then
+    rm chinchillas_show_site.tar.gz
+fi
+
+pushd chinchillas_show_site-linux
+zip chinchillas_show_site.tar.gz -r *
+mv chinchillas_show_site.tar.gz ../
+popd
