c6e8134a22ead685198a6024f060e1b7edc8b6dc
Stephan Kern <stephan@swmmrman.com>
2022-10-18 02:41:55 -0600

    Fixing wrong file and includeing new stuff

--- make_linux_rpi.sh
+++ make_linux_rpi.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-cross build --release --target=armv7-unknown-linux-gnueabihf
+cross build --release --target=aarch64-unknown-linux-gnu
 if [ -e chinchillas_show_site-linux-rpi ]; then
     rm -r chinchillas_show_site-linux-rpi
 fi
@@ -8,13 +8,12 @@ fi
 mkdir chinchillas_show_site-linux-rpi
 cp -r public_html chinchillas_show_site-linux-rpi
 cp -r template chinchillas_show_site-linux-rpi
-cp target/release/chinchillas_show_site chinchillas_show_site-linux-rpi/
+cp Rocket.toml chinchillas_show_site-linux-rpi
+cp show_info.toml chinchillas_show_site-linux-rpi
+cp target/aarch64-unknown-linux-gnu/release/chinchillas_show_site chinchillas_show_site-linux-rpi/
 
 if [ -e chinchillas_show_site-rpi.tar.gz ]; then
     rm chinchillas_show_site-rpi.tar.gz
 fi
 
-pushd chinchillas_show_site-linux-rpi
-zip chinchillas_show_site-rpi.tar.gz -r *
-mv chinchillas_show_site-rpi.tar.gz ../
-popd
+tar -czf chinchillas_show_site-rpi.tar.gz chinchillas_show_site-linux-rpi/
