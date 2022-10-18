dda53a4042d4d409844ea52570cde81b09691fe7
Stephan Kern <stephan@swmmrman.com>
2022-10-17 22:26:09 -0600

    Structs

--- src/config/mod.rs
+++ src/config/mod.rs
@@ -1,15 +1,19 @@
 use toml;
-use rocket::serde;
+use serde;
 
 struct config {
     show_info: show,
     branch_info: branch,
 }
 struct show {
-    
+    year: u32,
+    show_type: String,
+    date: String,
+    start_time: String,
+    end_time: String,
 }
 struct branch {
-
+    branch_name: String,
 }
 
 pub fn load_config() {
