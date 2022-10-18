429184a8a75b57ec34eef37eb03f03aac981fa02
Stephan Kern <stephan@swmmrman.com>
2022-10-17 22:22:50 -0600

    Building up structs

--- src/config/mod.rs
+++ src/config/mod.rs
@@ -1,5 +1,16 @@
-//use toml;
-//use serde;
+use toml;
+use rocket::serde;
+
+struct config {
+    show_info: show,
+    branch_info: branch,
+}
+struct show {
+    
+}
+struct branch {
+
+}
 
 pub fn load_config() {
     let config = std::fs::read_to_string("show_info.toml").unwrap();
